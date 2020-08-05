bff8bd13906a9b4d6e7368301736e7dbee53d0c6
rafid1841 <66725367+rafid1841@users.noreply.github.com>
2020-08-05 13:15:17 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+pkg install python2 -y
+pkg install python3 -y
+pkg install python-pip
+pkg install curl -y
+pkg install termux-api -y
+pip3 install mime
+pip3 install colorama
+pip3 install requests
+pip3 install bs4
+
